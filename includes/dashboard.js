d2734f6ec58282402c8f8763f54f24e443d16401
corsacca <corsacca@gmail.com>
2019-09-06 14:11:47 +0100

    And functionality to accept and decline buttons.

--- includes/dashboard.js
+++ includes/dashboard.js
@@ -7,18 +7,31 @@ jQuery(document).ready(function($) {
   $('#needs_accepting').html(data.accept_needed.total)
   let needs_accepting_list = ``
   data.accept_needed.contacts.slice(0, 3).forEach( contact =>{
-    needs_accepting_list += `<div style="margin-top:10px; display: flex">
+    needs_accepting_list += `<div style="margin-top:10px; display: flex" id="pending-${contact.ID}">
         <div style="display: inline-block; margin-left: 10px; vertical-align: middle; flex-grow: 1"><i class="fi-torso medium"></i>
             <a style="font-size: 1.1rem" href="${wpApiDashboard.site_url}/contacts/${contact.ID}">${_.escape(
       contact.post_title)}</a>
         </div>
         <div>
-            <button class="button small dt-green" style="color: white; margin-bottom: 0">${_.escape(wpApiDashboard.translations.accept)}</button>
-            <button class="button small" style="background-color: #f43636; color: white; margin-bottom: 0">${_.escape(wpApiDashboard.translations.decline)}</button>
+            <button class="button small dt-green accept_contact_button" data-id="${contact.ID}" data-action="accept" style="color: white; margin-bottom: 0">${_.escape(wpApiDashboard.translations.accept)}</button>
+            <button class="button small accept_contact_button" data-id="${contact.ID}" data-action="decline" style="background-color: #f43636; color: white; margin-bottom: 0">${_.escape(wpApiDashboard.translations.decline)}</button>
         </div>
     </div>`
   })
   $('#needs_accepting_list').html( needs_accepting_list )
+
+
+  $( document ).on('click', '.accept_contact_button', function () {
+    let contact_id = $(this).data('id')
+    let data = {accept: $(this).data('action') === 'accept'}
+    makeRequestOnPosts( "POST", `contacts/${contact_id}/accept`, data)
+      .then(function (resp) {
+        $(`#pending-${contact_id}`).remove()
+        let total_pending_html = $('#needs_accepting')
+        let total = parseInt( total_pending_html.html() ) - 1;
+        total_pending_html.html(total)
+      })
+  })
 
   /**
    * Update Needed
