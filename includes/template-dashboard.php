759ee55ab8e3bd958a24bc2844ed4acb1ccd09b0
corsacca <corsacca@gmail.com>
2019-09-06 09:46:45 +0100

    Set link to availability section

--- includes/template-dashboard.php
+++ includes/template-dashboard.php
@@ -82,10 +82,16 @@ get_header();
                         </div>
                         <p><?php esc_html_e( "Choose an option to let the dispatcher(s) know if you are ready for new contacts", 'disciple_tools' ) ?></p>
                         <?php $options = dt_get_site_custom_lists()["user_workload_status"] ?? [];
-                        foreach ( $options as $option_key => $option_val ) : ?>
-                        <button class="button hollow status-button" name="<?php echo esc_html( $option_key ) ?>"><?php echo esc_html( $option_val["label"] )?></button>
+                        foreach ( $options as $option_key => $option_val ) :
+                            $icon = $option_key === "active" ? "play" : ( $option_key === "existing" ? "pause" : "stop" ); ?>
+                            <button class="button hollow status-button" name="<?php echo esc_html( $option_key ) ?>">
+                                <i class="fi-<?php echo esc_html( $icon ) ?>"></i> <?php echo esc_html( $option_val["label"] )?>
+                            </button>
                         <?php endforeach; ?>
-<!--                        <a href="">--><?php //esc_html_e( "Set travel/unavailability", 'disciple_tools' ) ?><!--</a>-->
+                        <a href="<?php echo esc_html( site_url( 'settings/#availability' ) ) ?>">
+                            <i class="fi-clock"></i>
+                            <?php esc_html_e( "Set travel or dates unavailable", 'disciple_tools' ) ?>
+                        </a>
                     </div>
                 </div>
 
