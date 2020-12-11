88860476d2de31dd1795d4e3c77cc43a2f6f5a94
corsacca <corsacca@gmail.com>
2020-12-11 15:03:29 +0100

    Fix redirect on template load if the user is not logged in.

--- includes/template-dashboard.php
+++ includes/template-dashboard.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 $url = dt_get_url_path();
 $dt_post_type = explode( "/", $url )[0];
 
+dt_please_log_in();
+
 if ( ! current_user_can( 'access_contacts' ) ) {
     wp_die( esc_html( "Permission denied" ), "Permission denied", 403 );
 }
