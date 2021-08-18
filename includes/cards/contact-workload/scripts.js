21e6a6af2199af4a32625753fbd9ecf2f7b43ed0
Jennifer <jnmurrin@gmail.com>
2021-08-18 11:23:28 -0500

    Updated Contact Workload card styles

--- includes/cards/contact-workload/scripts.js
+++ includes/cards/contact-workload/scripts.js
@@ -2,12 +2,12 @@
   window.dt_dashboard.onAdd('DT_Dashboard_Plugin_Contact_Workload', function (context) {
     let status_buttons =  $(context.element).find('.status-button')
     let color_workload_buttons = (name) =>{
-      status_buttons.css('background-color', "")
+      // status_buttons.css('background-color', "")
       status_buttons.addClass("hollow")
       if ( name ){
         let selected =  $(context.element).find(`.status-button[name=${name}]`)
         selected.removeClass("hollow")
-        selected.css('background-color', window.lodash.get(context.wpApiDashboard, `workload_status_options.${name}.color`))
+        // selected.css('background-color', window.lodash.get(context.wpApiDashboard, `workload_status_options.${name}.color`))
         selected.blur()
       }
     }
@@ -21,7 +21,7 @@
         .then(()=>{
           $(context.element).find("#workload-spinner").removeClass("active")
         }).fail(()=>{
-        status_buttons.css('background-color', "")
+        // status_buttons.css('background-color', "")
         $(context.element).find("#workload-spinner").removeClass("active")
         status_buttons.addClass("hollow")
       })
