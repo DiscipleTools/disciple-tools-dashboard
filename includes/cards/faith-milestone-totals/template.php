cf42eac8061038ffd2512046abfcdc1cd70787e6
Jennifer <jnmurrin@gmail.com>
2021-08-20 08:43:06 -0500

    Update Faith Milestone Totals card styles

--- includes/cards/faith-milestone-totals/template.php
+++ includes/cards/faith-milestone-totals/template.php
@@ -4,7 +4,11 @@
           class="stats-spinner loading-spinner active"></div>
 </div>
 <div class="card-body">
-     <p style="text-align: center; margin-bottom: 30px"><?php esc_html_e("Milestones on your active contacts", 'disciple-tools-dashboard') ?></p>
-     <div style="display: flex; flex-wrap: wrap"
-     id="milestones"></div>
+     <p>
+          <?php esc_html_e("Milestones on your active contacts", 'disciple-tools-dashboard') ?>
+     </p>
+     <div id="milestones"></div>
 </div>
+
+
+
