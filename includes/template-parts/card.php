48b1203c81fabcc2d5ca5a118261cd635ed3f3bb
Jennifer <jnmurrin@gmail.com>
2021-09-20 10:28:05 -0500

    Update task card

--- includes/template-parts/card.php
+++ includes/template-parts/card.php
@@ -7,15 +7,23 @@
         <?php $card->render(); ?>
 
           <div class="card-footer">
-               <span class="card-nav-btn" onclick="toggleCardNav('<?php echo $card->handle; ?>')">x</span>
+               <span class="card-nav-btn" onclick="toggleCardNav('<?php echo $card->handle; ?>')">
+                    <i class="fas fa-ellipsis-v"></i>...
+               </span>
 
                <div class="card-nav" id="card-nav-<?php echo $card->handle; ?>">
           <div class="card-move-left"
-               onclick="dt_dashboard.moveBack('<?php echo $card->handle; ?>')">< Move Left</div>
+               onclick="dt_dashboard.moveBack('<?php echo $card->handle; ?>')">
+               <i class="fi-arrow-left"></i>
+               Move Left</div>
           <div class="card-move-right"
-               onclick="dt_dashboard.moveForward('<?php echo $card->handle; ?>')">> Move Right</div>
+               onclick="dt_dashboard.moveForward('<?php echo $card->handle; ?>')">
+               <i class="fi-arrow-right"></i> Move Right
+          </div>
           <div class="card-remove"
-               onclick="dt_dashboard.remove('<?php echo $card->handle; ?>')">X Remove Tile</div>
+               onclick="dt_dashboard.remove('<?php echo $card->handle; ?>')">
+               <i class="fi-x"></i> Remove Tile
+          </div>
           </div>
           </div>
 
