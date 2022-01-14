d388187be55470274daa499fbbefeca482a2b267
Jennifer <jnmurrin@gmail.com>
2022-01-14 15:57:00 -0600

    pending contacts style update

--- includes/cards/pending-contacts/template.php
+++ includes/cards/pending-contacts/template.php
@@ -1,7 +1,4 @@
 <div class="card-header">
-    <div class="count-square">
-        <span id="needs_accepting"></span>
-    </div>
     <span class="card-title">
         <?php echo $card->label ?>
     </span>
