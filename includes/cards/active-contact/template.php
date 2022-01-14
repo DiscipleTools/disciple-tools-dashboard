ecaebd2a7e79882632a7d46fa926bb049561e41c
Jennifer <jnmurrin@gmail.com>
2022-01-14 12:13:35 -0600

    active contact update

--- includes/cards/active-contact/template.php
+++ includes/cards/active-contact/template.php
@@ -2,12 +2,8 @@
     <?php echo $card->label ?>
 </div>
 <div class="card-body">
-    <div style="text-align: center; flex-grow: 1; margin-top: 40px">
-        <span class="numberCircle">&nbsp;<span id="active_contacts">-</span>&nbsp;</span>
-    </div>
-    <div class="view-all">
-        <a class="button" href="<?php echo esc_url(home_url('/')) . "contacts/new" ?>">
-            <?php esc_html_e("Add a contact", 'disciple-tools-dashboard') ?>
-        </a>
-    </div>
+    <span class="numberCircle">&nbsp;<span id="active_contacts">-</span>&nbsp;</span>
+    <a class="view-all button" href="<?php echo esc_url(home_url('/')) . "contacts/new" ?>">
+        <?php esc_html_e("Add a contact", 'disciple-tools-dashboard') ?>
+    </a>
 </div>
